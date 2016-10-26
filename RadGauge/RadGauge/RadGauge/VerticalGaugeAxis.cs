ae4065f850844cd11cb98922eb8cf02da35d4b38
Rosy Topchiyska <rtopchiyska@gmail.com>
2016-10-26 15:21:31 +0300

    Axis

---
+++ RadGauge/RadGauge/RadGauge/VerticalGaugeAxis.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SkiaSharp;
+
+namespace RadGauge
+{
+    public class VerticalGaugeAxis : VerticalGaugePart
+    {
+        double min = 0;
+        double max = 100;
+        double step = 10;
+
+
+        public VerticalGaugeAxis()
+        {
+
+        }
+
+        public SKRect Measure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetOffset()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override void Render(SKCanvas canvas, SKRect layoutSlot)
+        {
+
+        }
+    }
+}
