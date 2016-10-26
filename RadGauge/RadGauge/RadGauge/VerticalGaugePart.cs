3c3eee8101a14189e4ff8a57455f7b360e801a6b
Vesselin Georgiev <vesselin.georgiev@telerik.com>
2016-10-26 15:01:19 +0300

    Added VerticalGaugePart abstract class

---
+++ RadGauge/RadGauge/RadGauge/VerticalGaugePart.cs
@@ -0,0 +1,14 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadGauge
+{
+    public abstract class VerticalGaugePart
+    {
+        public abstract void Render(SKCanvas canvas, SKRect layoutSlot);
+    }
+}
