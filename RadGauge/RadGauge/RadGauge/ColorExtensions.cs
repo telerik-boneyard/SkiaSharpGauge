6d65cbc082b54d6c6460cf7240386120e776798d
Nikolay Demirev <demirev@progress.com>
2016-10-26 16:50:10 +0300

    Implemented vertical gauge ranges

---
+++ RadGauge/RadGauge/RadGauge/ColorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using SkiaSharp;
+using Xamarin.Forms;
+
+namespace RadGauge
+{
+    public static class ColorExtensions
+    {
+        public static SKColor ToSKColor(this Color color)
+        {
+            return new SKColor(
+                    (byte)(color.R * 255),
+                    (byte)(color.G * 255),
+                    (byte)(color.B * 255),
+                    (byte)(color.A * 255));
+        }
+    }
+}
--- RadGauge/RadGauge/RadGauge/VerticalGaugeRangesRenderer.cs
+++ RadGauge/RadGauge/RadGauge/VerticalGaugeRangesRenderer.cs
@@ -10,16 +10,40 @@ namespace RadGauge
 {
     public class VerticalGaugeRangesRenderer : VerticalGaugePart
     {
-        //private double min = 0;
-        //private double max = 100;
-        //private double step = 20;
-        //private double value = 22;
-        //private double[] ranges = { 0, 20, 40, 100 };
-        //private Color[] ranges = { Color.Green, Color.FromRgb( };
+        private float min = 0;
+        private float max = 100;
+        private float[] ranges = { 0, 33, 66, 100 };
+        private Color[] colors = { Color.White, Color.Green, Color.Red };
 
         public override void Render(SKCanvas canvas, SKRect layoutSlot)
         {
-            
+            float totalHeight = layoutSlot.Height;
+
+            float top = layoutSlot.Top;
+
+            for (int i = 0; i < ranges.Length - 1; i++)
+            {
+                float ratio = (max - min) / (ranges[i + 1] - ranges[i]);
+
+                float rangeHeight = totalHeight / ratio;
+
+                SKRect rect = new SKRect(layoutSlot.Left, top, layoutSlot.Right, top + rangeHeight);
+                top = top + rangeHeight;
+
+                Color color = colors[i];
+                DrawRectangle(canvas, rect, color);
+            }
+        }
+
+        private static void DrawRectangle(SKCanvas canvas, SKRect rect, Color xamarinColor)
+        {
+            using (SKPaint paint = new SKPaint())
+            {
+                paint.Color = xamarinColor.ToSKColor();
+                paint.IsAntialias = true;
+
+                canvas.DrawRect(rect, paint);
+            }
         }
 
         internal SKSize Measure(SKSize availableSize)
